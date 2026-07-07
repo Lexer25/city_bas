a71222d91b3c056bc6739896962f0b10db45a47d
Андрей <andrey.buharov@gmail.com>
2026-07-07 23:57:07 +0300

    Убрал ненужный роут

--- init.php
+++ init.php
@@ -2,11 +2,6 @@
 // MODPATH/about/init.php
 defined('BAS_VERSION') OR define('BAS_VERSION', '2.0.0');
 
-Route::set('default', '(<controller>(/<action>(/<id>)))')
-	->defaults(array(
-		'controller' => 'dashboard',
-		'action'     => 'index',
-	));
 	
 	
 Kohana::$config->load('menu')
